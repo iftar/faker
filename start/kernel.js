0ce8977a49731b3b14f8a54b5545fbb09ce09bd1
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2020-04-15 21:47:12 +0100

    update cors

--- start/kernel.js
+++ start/kernel.js
@@ -15,7 +15,7 @@ const Server = use('Server')
 const globalMiddleware = [
   'Adonis/Middleware/BodyParser',
   'Adonis/Middleware/Session',
-  //'Adonis/Middleware/Shield',
+  'Adonis/Middleware/Shield',
   'Adonis/Middleware/AuthInit',
   'App/Middleware/ConvertEmptyStringsToNull',
 ]
@@ -54,7 +54,7 @@ const namedMiddleware = {
 */
 const serverMiddleware = [
   'Adonis/Middleware/Static',
-  //'Adonis/Middleware/Cors'
+  'Adonis/Middleware/Cors'
 ]
 
 Server
