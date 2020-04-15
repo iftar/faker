eda67b14df00968ca7739371b8be3bbc3575c604
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2020-04-15 21:10:05 +0100

    Adds login

--- README.md
+++ README.md
@@ -1,31 +1,11 @@
-# Adonis fullstack application
+# Iftar Faker
 
-This is the fullstack boilerplate for AdonisJs, it comes pre-configured with.
+This repo fakes API requests to the Share Iftar platform. This allows frontend developers to build the user interface without having to wait for new api changes.
 
-1. Bodyparser
-2. Session
-3. Authentication
-4. Web security middleware
-5. CORS
-6. Edge template engine
-7. Lucid ORM
-8. Migrations and seeds
+## APIs
 
-## Setup
+**Login**
 
-Use the adonis command to install the blueprint
-
-```bash
-adonis new yardstick
 ```
-
-or manually clone the repo and then run `npm install`.
-
-
-### Migrations
-
-Run the following command to run startup migrations.
-
-```js
-adonis migration:run
+POST /api/login
 ```
--- app/Controllers/Http/AuthApiController.js
+++ app/Controllers/Http/AuthApiController.js
@@ -17,76 +17,24 @@ class AuthApiController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
-
-  /**
-   * Render a form to be used for creating a new authapi.
-   * GET authapis/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
-
-  /**
-   * Create/save a new authapi.
-   * POST authapis
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
-  }
-
-  /**
-   * Display a single authapi.
-   * GET authapis/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing authapi.
-   * GET authapis/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
-
-  /**
-   * Update authapi details.
-   * PUT or PATCH authapis/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
-  }
-
-  /**
-   * Delete a authapi with id.
-   * DELETE authapis/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {
+  async login ({ request, response, view }) {
+    response.json({
+      "status": "success",
+      "data": {
+          "user": {
+              "id": 31,
+              "first_name": "John",
+              "last_name": "Smith",
+              "email": "john.smith@gmail.com",
+              "email_verified_at": "2020-04-14T23:30:36.000000Z",
+              "type": "user",
+              "status": "approved",
+              "created_at": "2020-04-14T23:29:29.000000Z",
+              "updated_at": "2020-04-14T23:30:36.000000Z"
+          },
+          "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiOWM1M2YxNDI5ZTlkMWRjZjUxYWNiNDA5OWVmYmJiMDQ2NzI4OTYwNDU4ZjYzNmRhZmY3YmExMmE0YjcyYjQ5MzI2NmMzOGY0YTllMWNkNWUiLCJpYXQiOjE1ODY5ODAyOTIsIm5iZiI6MTU4Njk4MDI5MiwiZXhwIjoxNjE4NTE2MjkyLCJzdWIiOiIzMSIsInNjb3BlcyI6W119.UizFAOQ0Nwd8waUd6vva30s9c6ttATZ2omzRa79q_XcNoqNLPv3JzvyMOvJQG_nAqqBHHHjkzCJMielxOa5kGfYbY94mJORq6k-Fr2FlCamt3hwP2SNe5igroxkZCfIYlyUde2ygLpmU-Hvc9ihAsu4IBvxxyD_Ylue-gBeGGUvsrVYSaWXqFI6gg_ZCmHZfrfGqrMlJXl6lzZRXxnljHoRj8QJMSXxuebWCHvaz75HR4Wn503chjHM9uuQAsGkFPQRYZyKDNxmr0o3IG7LxfXhFFKh6aKHAsS3DL1v_AclpFXCxqwRbmfl0h_c7MK5EuBvOh3vbu756PerYu_CylVqmo85lQ-jFqH7qxtNjlbT6WlMRO0oxFm47zIHUW0fkDBkuRPSscGoE0cajrNxmnQi6sLUwgjCg_4Oi2iiLJpeQ92bhxAudbq_PLulQTTsFljCQLd8LQghjuyEJBofSSefEEME566wtK9st3Fr2Tobv5K2HFOVTerQLzv6UlPjyuxxx-zC9Rhy1Xm9FXhFCJc5yw2xaKEffnSiCUYzM8Q1hl2MzRteaQAgcTr4YzexgDfMN9n5SL94RxtQe1NHT8NvHGqgdKFTcU7djFtoqdJUHlQ_4oVCLABtkixyPpa1Ei07V8cqj_Ut7oO0ZhWpxM0hsOZ0kVW3B81BHzaoDVFI"
+      }
+    });
   }
 }
 
